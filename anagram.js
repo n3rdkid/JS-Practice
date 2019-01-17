2ff5357df309e6cc23457782d63bc9fa09b6b836
Saurav Adhikari <sauravads123@gmail.com>
2019-01-17 18:37:20 +0545

    Complete Anagram problem using Frequency Counter

---
+++ anagram.js
@@ -0,0 +1,32 @@
+/*
+	A program to detect anagrams
+
+	anagramTest('anagram','nanagram');
+	*/
+
+	function anagramTest(word,testWord)
+	{	
+		console.time("Start");
+		const letterCountOfWord={};
+		const letterCountOfTestWord={};
+
+		for(let letter of word)
+		{
+			letterCountOfWord[letter]=(letterCountOfWord[letter] || 0) + 1;
+		}
+		for(let letter of testWord)
+		{
+			letterCountOfTestWord[letter]=(letterCountOfTestWord[letter] || 0) + 1;
+		}
+		let isAnagram=true;
+		for(let key in letterCountOfTestWord)
+		{
+			if(!(letterCountOfWord.hasOwnProperty(key)&&(letterCountOfTestWord[key]<=letterCountOfWord[key])))
+				{		isAnagram=false;
+					break;
+				}
+			}
+			console.log(`${testWord} is anagram of ${word} : ${isAnagram}`);
+			console.timeEnd("Start");
+		}
+		anagramTest("hello","ello");
